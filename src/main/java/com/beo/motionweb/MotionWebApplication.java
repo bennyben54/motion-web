4f184bf4db03dab47c1177382c37337b137ddf18
bennyben54 <bennyben54@gmail.com>
2019-05-11 12:36:58 +0200

    [CHORE](init) init mvn project

---
+++ src/main/java/com/beo/motionweb/MotionWebApplication.java
@@ -0,0 +1,13 @@
+package com.beo.motionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotionWebApplication.class, args);
+	}
+
+}
